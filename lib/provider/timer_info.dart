24756a01bb1ec9d303c584e48c2462d4bef9abba
Dhruv singh <49086525+dhruv9045@users.noreply.github.com>
2021-10-26 22:22:01 +0530

    Update timer_info.dart

--- lib/provider/timer_info.dart
+++ lib/provider/timer_info.dart
@@ -3,8 +3,8 @@ import 'package:flutter/foundation.dart';
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-int min = 3;
-int max = 6;
+int min = 30;
+int max = 60;
 Random random = new Random();
 
 class TimerInfo extends ChangeNotifier {
